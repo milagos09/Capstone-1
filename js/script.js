82c44436ddcdf3f96f770990ec680a10c4648ed5
milagos09 <milagos31@gmail.com>
2021-12-12 20:11:25 +0800

    rounded favicon

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//   alert("Mobile Detected");
+// } else alert("Non Mobile Device");
+
+// function getCoordinate() {
+//   const body = document.querySelector("html")
+//   console.log(body.getBoundingClientRect().y)
+//   }
